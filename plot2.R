84c1bb38e45b0bfab0c1fcd48beeb67575fc974f
interpretstudio <39842246+interpretstudio@users.noreply.github.com>
2018-11-02 11:26:38 +0000

    Plot 1 & 2

--- plot2.R
+++ plot2.R
@@ -1,5 +1,5 @@
-## This script loads the household power consumption data and produces a histogram
-## for global active power for the days 1st and 2nd February 2007.  The histogram
+## This script loads the household power consumption data and produces a line chart
+## for global active power over the days 1st and 2nd February 2007.  The chart
 ## is saved into a PNG file with the dimensions 480 x 480.
 
 cons <- read.table('household_power_consumption.txt',sep = ';', header=TRUE, stringsAsFactors = FALSE,
@@ -7,6 +7,7 @@ cons <- read.table('household_power_consumption.txt',sep = ';', header=TRUE, str
                    na.strings='?')
 cons$pDate <- as.Date(strptime(cons$Date,format='%d/%m/%Y'))
 cons <- subset(cons, pDate == '2007-02-01' | pDate == '2007-02-02')
-png('plot1.png',480,480)
-hist(cons$Global_active_power,xlab='Global Active Power (kilowatts)',main='Global Active Power',col='red')
+cons$pDateTime <- strptime(paste(cons$Date,cons$Time),format='%d/%m/%Y %X')
+png('plot2.png',480,480)
+with(cons,plot(pDateTime, Global_active_power, type='l', xlab='', ylab='Global Active Power (kilowatts)'))
 dev.off()
